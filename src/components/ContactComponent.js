90b7b8200943edc1d32578593cdca2a97523bfa8
John Paul Liberal <jliberal@gmail.com>
2020-08-25 21:50:18 -0400

    Redux React Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,7 +3,6 @@ import {
 	Breadcrumb,
 	BreadcrumbItem,
 	Button,
-	Form,
 	FormGroup,
 	Label,
 	Input,
@@ -12,7 +11,7 @@ import {
 	FormFeedback,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -37,6 +36,7 @@ class Contact extends Component {
 	handleSubmit(values) {
 		console.log('Current State is: ' + JSON.stringify(values));
 		alert('Current State is: ' + JSON.stringify(values));
+		this.props.resetFeedbackForm();
 	}
 
 	render() {
@@ -105,7 +105,10 @@ class Contact extends Component {
 						<h3>Send us your Feedback</h3>
 					</div>
 					<div className="col-12 col-md-9">
-						<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+						<Form
+							model="feedback"
+							onSubmit={(values) => this.handleSubmit(values)}
+						>
 							<Row className="form-group">
 								<Label htmlFor="firstname" md={2}>
 									First Name
@@ -267,7 +270,7 @@ class Contact extends Component {
 									</Button>
 								</Col>
 							</Row>
-						</LocalForm>
+						</Form>
 					</div>
 				</div>
 			</div>
